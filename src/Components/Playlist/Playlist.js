90d2ac69503863bdc4011974335e6684cc832821
wfercanas <fernando.canas@outlook.com>
2021-07-22 13:09:04 -0500

    add/ playlist name value bind to app state

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -17,7 +17,11 @@ export class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue="New Playlist" onChange={this.handleNameChange} />
+        <input
+          defaultValue="Create Playlist"
+          value={this.props.playlistName}
+          onChange={this.handleNameChange}
+        />
         <TrackList
           tracks={this.props.playlistTracks}
           onRemove={this.props.onRemove}
