543afa776b64879adf80e57f2c0d891706263960
wfercanas <fernando.canas@outlook.com>
2021-07-21 21:09:30 -0500

    add/ rendering list of tracks in SearchResult

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,21 +6,74 @@ import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'Name',
+          artist: 'Artist',
+          album: 'album',
+          id: '1',
+        },
+        {
+          name: 'Name',
+          artist: 'Artist',
+          album: 'album',
+          id: '2',
+        },
+        {
+          name: 'Name',
+          artist: 'Artist',
+          album: 'album',
+          id: '3',
+        },
+      ],
+      playlistName: 'My Favorites',
+      playlistTrack: [
+        {
+          name: 'First song',
+          artist: 'Ed Sheeran',
+          album: 'the album',
+          id: '11',
+        },
+        {
+          name: 'Second song',
+          artist: 'Ed Sheeran',
+          album: 'the album',
+          id: '12',
+        },
+        {
+          name: 'Third song',
+          artist: 'Ed Sheeran',
+          album: 'the album',
+          id: '13',
+        },
+        {
+          name: 'Fourth song',
+          artist: 'Ed Sheeran',
+          album: 'the album',
+          id: '14',
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults results={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -15,9 +15,9 @@ export class Track extends React.Component {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3>{/* <!-- track name will go here -->*/}</h3>
+          <h3>{this.props.track.name}</h3>
           <p>
-            {/* <!-- track artist will go here--> | <!-- track album will go here -->*/}
+            {this.props.track.artist} {this.props.track.album}
           </p>
         </div>
         <button className="Track-action">{this.renderAction()}</button>
